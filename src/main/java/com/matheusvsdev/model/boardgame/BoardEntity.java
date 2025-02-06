e5ef9bf9dca8809f97a91f6beb4098bbd9b76dd0
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2025-02-06 17:07:37 -0300

    BoardException and defensive programming

--- src/main/java/com/matheusvsdev/model/boardgame/BoardEntity.java
+++ src/main/java/com/matheusvsdev/model/boardgame/BoardEntity.java
@@ -6,37 +6,56 @@ public class BoardEntity {
     private PieceEntity[][] pieces;
 
     public BoardEntity(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Rows and columns must be greater than 0");
+        }
         this.rows = rows;
         this.columns = columns;
         this.pieces = new PieceEntity[rows][columns];
     }
 
     public PieceEntity piece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[row][column];
     }
 
     public PieceEntity piece(PositionEntity position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(PieceEntity piece, PositionEntity position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece on position " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    public boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(PositionEntity position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(PositionEntity position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+        return piece(position) != null;
     }
 
     public int getRows() {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
-    }
-
-    public void setColumns(int columns) {
-        this.columns = columns;
     }
 }
---
+++ src/main/java/com/matheusvsdev/model/boardgame/BoardException.java
@@ -0,0 +1,8 @@
+package com.matheusvsdev.model.boardgame;
+
+public class BoardException extends RuntimeException {
+
+    public BoardException(String message) {
+        super(message);
+    }
+}
